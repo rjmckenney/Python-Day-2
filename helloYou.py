fbea52935b410873542a6cbe3f0ab2d5640194ed
rjmckenney <janellemckenney17@gmail.com>
2019-08-30 15:46:35 -0400

    Add helloYou

---
+++ helloYou.py
@@ -0,0 +1,10 @@
+#name = input('What is your name?')
+#print(str.upper(str('hello ' + name)))
+#print('Your name has' + str(len(name)) + 'letters in it! Awesome!')
+
+user_input = int(input("Enter a number:"))
+result = user_input * user_input
+print(result )
+
+
+
